924b3ae5d7443bb56633eb146c22eee01ab27719
Ofekraf <Ofek.Rafaeli@mail.huji.ac.il>
2020-11-30 13:11:30 +0200

    cleaned a bit chunk of the data

--- env.py
+++ env.py
@@ -1,2 +1,10 @@
+import os
 
+PRE_PROCESS_ORIGINAL_SENTENCES = True
+ORIG_SENT_FILE_NAME = "original_but_sentences"
+CLEAN_SENT_FILE_NAME = "clean_sentences"
+ORIGINAL_BUT_SENTENCES_PATH = os.path.join(os.getcwd(), ORIG_SENT_FILE_NAME)
+CLEAN_SENT_FILE_LOCATION = os.path.join(os.getcwd(), CLEAN_SENT_FILE_NAME)
+SENTENCE_TYPE_DELIMITER = "####$$$$####%%%%%#####|||||"
 
+LOG = True
